c14b1a0b7d49c8cae6020875e663fce6512b050a
Tiago C M Matos <tcm.matos@gmail.com>
2023-10-27 13:53:51 +0000

    quick code refactor

--- minilinter.js
+++ minilinter.js
@@ -1,46 +1,21 @@
-let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+const story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+const overusedWords = ['really', 'very', 'basically'];
+const unnecessaryWords = ['extremely', 'literally', 'actually' ];
 
-let overusedWords = ['really', 'very', 'basically'];
+const storyWords = story.split(' ');
 
-let unnecessaryWords = ['extremely', 'literally', 'actually' ];
-
-let storyWords = story.split(' ');
-
-let betterWords = storyWords.filter(word => {
-
-  let betterWord = true;
-
-  unnecessaryWords.forEach(unnecessaryWord => {
-    if (unnecessaryWord == word) {
-      betterWord = false;
-    }
-  });
-
-  return betterWord;
+const betterWords = storyWords.filter(word => {
+  const isAnyUnncessaryWordFound = unnecessaryWords.some(unnecessaryWord => unnecessaryWord === word);
+  return !isAnyUnncessaryWordFound;
 });
 
-let overusedWordsCount = storyWords.filter(word => {
-
-  let overused = false;
-
-  overusedWords.forEach(overusedWord => {
-    if (overusedWord == word) {
-      overused = true;
-    }
-  })
-
-  return overused;
+const overusedWordsCount = storyWords.filter(word => {
+  const isAnyOverusedWordFound = overusedWords.some(overusedWord => overusedWord === word);
+  return isAnyOverusedWordFound;
 });
 
 let sentenceCounter = storyWords.filter(word => {
-
-  let isSentenceEnd = false;
-
-  if (word.charAt(word.length - 1) == '!'
-      || word.charAt(word.length - 1) == '.') {
-    isSentenceEnd = true;
-  }
-
+  const isSentenceEnd = (word.charAt(word.length - 1) == '!' || word.charAt(word.length - 1) == '.');
   return isSentenceEnd;
 });
 
